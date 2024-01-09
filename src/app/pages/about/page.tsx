aceb9ca0cd3498ced5a92cf30f7346b6478d80c4
TedCreator <takahori2@gmail.com>
2024-01-09 00:15:34 -0500

    Added content to About page

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -35,9 +35,54 @@ export default function About() {
           </Link>
         </div>
       </header>
-      <main >
-        About me:
-
+      <main>
+        <table  id="aboutTable">
+          <tr>
+            <td>
+              <h3 className='aboutLabel'>Life</h3>
+              <hr />
+            </td>
+          </tr>
+          <tr>
+            <td className='aboutContent'>
+              I am a Japanese-American student at University of South Carolina, and I go by my nickname Ted.
+              I was born and raised around Columbia, South Carolina and I've lived here all my life.
+              My family often travels, we love Myrtle Beach, Disney, and the Smokey Mountains.
+              It used to be just me and my family, but recently as we've gotten older, our friends have been coming with us.
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td>
+              <h3 className='aboutLabel'>Experience</h3>
+              <hr />
+            </td>
+          </tr>
+          <tr>
+            <td className='aboutContent'>
+              Though I am still working on getting work experience, as I have said I'm working on developing my skills with
+              various languages, algorithms, and data structures to create more robust and streamlined apps.
+              Currently, I'm learning User Interface frameworks such as 
+              Qt for my ZipUp app, PyQt6 for my Scheduled-Shutdown app, and React with NextJS for this site.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h3 className='aboutLabel'>Ambition</h3>
+              <hr />
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td className='aboutContent'>
+              I'm a very driven person, and at the moment the only thing I'm pursuing is my degree and a career.
+              To pair with my CS classes, I'm learning various useful languages and frameworks to be ready to work in the field.
+              Currently, I am pursuing an internship before I graduate college to get experience.
+              In the future my dream is to be a software developer, to work closely with a team of my peers to design and develop
+              both a functional and user-friendly app.
+            </td>
+          </tr>
+        </table>
       </main>
     </div>
   );
